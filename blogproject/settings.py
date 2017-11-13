bd9e98af82fe028cfaf67fbd5d618d06c6b4181e
syk <cirillaye@outlook.com>
2017-11-13 23:02:56 +0800

    modified about

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 've=odbozkj70-6nf1xq^fjeddm-2z(k#(^ec_i30+d)9egr^%k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
